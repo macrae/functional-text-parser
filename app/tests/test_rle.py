1c2bd737415ab248dab65c5324ae8e60a59f5ebd
Sean MacRae <sean.macrae@changehealthcare.com>
2017-12-16 17:06:04 -0800

    moved functions/ and tests/ within app/ directory so flask app could find our custom methods

--- app/tests/test_rle.py
+++ app/tests/test_rle.py
@@ -1,9 +1,9 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('./functions'))
+sys.path.insert(0, os.path.abspath('.'))
 
-from rle import *
+from app.functions.rle import *
 
 string = 'AABBCDDDE'
 
