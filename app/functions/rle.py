8891fcf523fc75b4b2581bd90d1c4d39f29cdc4a
Sean MacRae <sean.macrae@changehealthcare.com>
2019-04-01 13:50:58 -0700

    updating how pytest is configured

--- app/functions/rle.py
+++ app/functions/rle.py
@@ -1,102 +1,54 @@
-import numpy as np
 from functools import reduce
+from typing import List
+
+import numpy as np
 
 
-def listify_string(string):
+def listify_string(s: str) -> List[str]:
+    """Create a list of characters from a string
     """
-    Put each character in a string into a list.
-
-    Args:
-        string (Str):          string to listify
-
-    Returns:
-        list (Str):            list of characters in string
-    """
-    return list(map(lambda x: x, string))
+    return list(map(lambda x: x, s))
 
 
-def get_pairs(l):
-    """
-    Group consecutive elements of a list, into a list of 2-tuples.
-
-    Args:
-        list (Str):             list of elements
-
-    Returns:
-        list (Tuple):           list of 2-tuples
+def group_consecutive_elements(l: list) -> List[tuple]:
+    """Group consecutive elements of a list into a list of tuples
     """
     return list(zip(l, l[1:]))
 
 
-def compare_pairs(pairs):
+def compare_tuples(t: List[tuple]):
+    """Compare each tuple in a list (x, y) for equality (x == y).
     """
-    Compare each 2-tuple in a list (x, y) for equality (x == y).
+    return [x == 0 for x in list(map(lambda x: hash(x[0]) - hash(x[1]), t))]
 
-    Args:
-        list (Tuples):          list of 2-tuples
 
-    Returns:
-        list (Booleans):        list of booleans
+def get_break_points(pair_comparison: List[tuple]):
+    """Get index of tuple inequality (x != y) as indicator of character change.
     """
-    return [x == 0 for x in list(
-        map(lambda x: hash(x[0]) - hash(x[1]), pairs))]
-
-
-def get_break_points(pair_comparison):
-    """
-    Get index of 2-tuple inequality (x != y) as indicator of character change.
-
-    Args:
-        list (Booleans):          list of booleans
-
-    Returns:
-        list (Int):               list of indices for character changes
-    """
-    index_equal_pairs = list(
-        zip(np.arange(0, len(pair_comparison)), pair_comparison))
-
+    index_equal_pairs = list(zip(np.arange(0, len(pair_comparison)), pair_comparison))
     breaks = list(filter(lambda x: x[1] == False, index_equal_pairs))
-
     break_points = list(map(lambda x: x[0] + 1, breaks))
-
     break_points = [0] + break_points + [len(pair_comparison) + 1]
-
     return break_points
 
 
-def break_list(list, break_points):
-    """
-    Given a list and indices of consecutive character breaks for that list,
+def break_list(list: List, break_points):
+    """Given a list and indices of consecutive character breaks for that list,
     generate a list of lists containing the consecutive characters.
-
-    Args:
-        list (String):                 list of characters in a string
-        break_points (List[Int])
-
-    Returns:
-        list (Tuples):            list of 2-tuples
     """
     new_list = []
-
-    for item in get_pairs(break_points):
-        new_list.append(list[item[0]:item[1]])
-
+    for item in group_consecutive_elements(break_points):
+        new_list.append(list[item[0] : item[1]])
     return new_list
 
 
 def aggregate_broken_list(broken_list):
-
     list_of_counts = list(map(lambda x: str(len(x)) + str(x[0]), broken_list))
-
     string_of_counts = reduce(lambda accum, elem: accum + elem, list_of_counts)
-
     return string_of_counts
 
 
 def rle(string):
-
     l = listify_string(string)
-
-    break_points = get_break_points(compare_pairs(get_pairs(l)))
-
+    break_points = get_break_points(compare_tuples(group_consecutive_elements(l)))
     return aggregate_broken_list(break_list(l, break_points))
--- app/tests/test_rle.py
+++ app/tests/test_rle.py
@@ -15,28 +15,28 @@ def test_listify_string():
 
 
 def test_get_pairs():
-    pairs = get_pairs(listify_string(string))
+    pairs = group_consecutive_elements(listify_string(string))
 
     assert pairs == [('A', 'A'), ('A', 'B'), ('B', 'B'), ('B', 'C'),
                      ('C', 'D'), ('D', 'D'), ('D', 'D'), ('D', 'E')]
 
 
 def test_compare_pairs():
-    comparisons = compare_pairs(get_pairs(listify_string(string)))
+    comparisons = compare_tuples(group_consecutive_elements(listify_string(string)))
 
     assert comparisons == [True, False, True, False, False, True, True, False]
 
 
 def test_get_break_points():
     break_points = get_break_points(
-        compare_pairs(get_pairs(listify_string(string))))
+        compare_tuples(group_consecutive_elements(listify_string(string))))
 
     assert break_points == [0, 2, 4, 5, 8, 9]
 
 
 def test_break_list():
     break_points = get_break_points(
-        compare_pairs(get_pairs(listify_string(string))))
+        compare_tuples(group_consecutive_elements(listify_string(string))))
 
     broken_list = break_list(listify_string(string), break_points)
 
@@ -46,7 +46,7 @@ def test_break_list():
 
 def test_aggregate_broken_list():
     break_points = get_break_points(
-        compare_pairs(get_pairs(listify_string(string))))
+        compare_tuples(group_consecutive_elements(listify_string(string))))
 
     broken_list = break_list(listify_string(string), break_points)
 
